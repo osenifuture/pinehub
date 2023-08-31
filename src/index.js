4692e27d6e5badce8f2b814ca05aafb99ec5a663
user <user@users-MacBook-Pro.local>
2023-08-30 17:59:00 -0700

    fix normalize

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import "normalize.css"
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './Context/AuthContext';
+import "normalize.css"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
